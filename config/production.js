0d3dd018aad5c1103fdc203d6d7140a3ac66d1d9
Daniel Rio Christian <daniel41697@gmail.com>
2021-11-09 15:32:36 +0700

    change provision key for production

--- config/production.js
+++ config/production.js
@@ -3,7 +3,7 @@ module.exports = {
     clientId: '1013140c470ccbdb.servicebuss.transvision.co.id',
     clientSecret: '5dd510be503f2a9d',
     clientGrantType: 'client_credentials',
-    provisionKey: 'CuT0xc9am8smutZkrRNSsTM2l9LOvOGw',
+    provisionKey: 'ustKBa35UvAVFocuTnMSpVN6IdajMD0u',
     authenticatedUserid: 'testing',
     secretCookiePassword: 'CuT0c9am8smuanindithatZkrRNSsTM2l9LanungOvOGw',
 };
