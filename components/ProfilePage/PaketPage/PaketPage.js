b1264d5deecb1a847ac43033797d38fa66377427
danielriochristian <daniel.christian@transvision.co.id>
2021-12-09 14:52:00 +0700

    mobile profile

--- components/ProfilePage/PaketPage/PaketPage.js
+++ components/ProfilePage/PaketPage/PaketPage.js
@@ -16,19 +16,19 @@ export default function PaketPage({minipack}) {
             {minipack?.data?.result.map((data) => (
                 <>
                     <div className="mt-12 w-60">
-                        <p className="text-xl text-gray-500">Paket {data.title}</p>
+                        <p className="text-lg lg:text-xl text-gray-500">Paket {data.title}</p>
                     </div>
                     <div className="grid grid-cols-2">
                         <div className="flex">
-                            <p className="self-center text-2xl" style={{ color: '#0285e4' }}>{sisa(data?.end_date)}</p>
-                            <p className="ml-4 self-center text-gray-500 text-2xl">Hari Tersisa</p>
+                            <p className="self-center text-xl lg:text-2xl" style={{ color: '#0285e4' }}>{sisa(data?.end_date)}</p>
+                            <p className="ml-4 self-center text-gray-500 text-xl lg:text-2xl">Hari Tersisa</p>
                         </div>
                         <div>
                             <div className="flex gap-x-4">
                                 <div className="rounded-xl flex flex-col bg-white w-36 h-36" style={{ boxShadow: '-7px 20px 100px 0 rgba(0, 0, 0, 0.1)' }}>
                                     <p className="my-4 text-center text-gray-500">Mulai</p>
                                     <div className="self-center text-center">
-                                        <p className="text-4xl text-gray-500">{format(new Date(data?.start_date), 'dd')}</p>
+                                        <p className="text-3xl lg:text-4xl text-gray-500">{format(new Date(data?.start_date), 'dd')}</p>
                                     </div>
                                     <div className="mt-2 text-center">
                                         <p className="text-gray-500"> {format(new Date(data?.start_date), 'MMM yyy')}</p>
@@ -38,7 +38,7 @@ export default function PaketPage({minipack}) {
                                 <div className="rounded-l-xl flex flex-col w-36 h-36" style={{ backgroundColor: '#0285e4' }}>
                                     <p className="my-4 text-center text-white">Berakhir</p>
                                     <div className="self-center text-center">
-                                        <p className="text-4xl text-white"> {format(new Date(data?.end_date), 'dd')}</p>
+                                        <p className="text-3xl lg:text-4xl text-white"> {format(new Date(data?.end_date), 'dd')}</p>
                                     </div>
                                     <div className="mt-2 text-center">
                                         <p className="text-white"> {format(new Date(data?.end_date), 'MMM yyy')}</p>
